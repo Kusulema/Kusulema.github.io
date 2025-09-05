2426850d62f5ed6a61d617cb6cf0056f35fbf694
Kirill Fjodorov <kirill.fjodorov@ivkhk.ee>
2025-09-05 10:06:47 +0300

    portfolio

---
+++ Drumm/script.js
@@ -0,0 +1,18 @@
+$(function() {
+    $(this).keydown(function(event) {
+        var key = $(this).find('.key[data-key="' + event.which + '"]');
+        var audio = $(this).find('audio[data-key="' + event.which + '"]')[0];
+
+        key.toggleClass('playing');
+
+        if (!audio) return;
+
+        audio.currentTime = 0;
+        audio.play();
+    });
+
+    $(this).keyup(function(event) {
+        var key = $(this).find('.key[data-key="' + event.which + '"]');
+        key.toggleClass('playing');
+    });
+});
